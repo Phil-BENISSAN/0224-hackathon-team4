94907451dc79a0da0a3cad3cd912e5705bd76955
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-07-03 15:18:56 +0200

    modified dockerfile and docker compose

--- dev-react/src/App.jsx
+++ dev-react/src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import './App.css'
 
 
@@ -5,6 +6,7 @@ function App() {
 
   return (
     <>
+    <Outlet />
      <h1 className=' text-rose-600'>HELLO WORLD</h1>
     </>
   );
