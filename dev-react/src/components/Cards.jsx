9d0e6bb89377ba98b1949609ddcc99d306cf4e1a
AXEL JANIAK <axeljpro@gmail.com>
2024-07-04 19:29:42 +0200

    nouvelles bases

--- dev-react/src/components/Cards.jsx
+++ dev-react/src/components/Cards.jsx
@@ -1,18 +1,14 @@
-import { Link } from "react-router-dom"
-import dataset from "../../Dataset Bordeaux.json"
+/* eslint-disable react/prop-types */
 
-function Cards() {
-  const data = dataset.Sheet1 || [];
-  const limitedData = data.slice(0, 10);
+
+
+function Cards({item}) {
   return (
-    <>
-    {limitedData.map((item) => (
-    <Link to="" key={item.id}>
-    <article className="flex w-[22rem] h-[10rem] p-8 rounded-2xl gap-3 shadow-lg items-center mb-1 overflow-hidden">
-            <img src="./src/assets/images/Plat céramique blanche.jpg" alt="test" className="w-32 h-32 rounded"/>
+    <article className="flex w-[22rem] h-[10rem] m-5 rounded-2xl gap-3 shadow-lg items-center mb-1 overflow-hidden">
+            <img src={`${item.url_image}`} alt="test" className="w-32 h-32 rounded"/>
             <span className="flex flex-col gap-3">
               <div>
-            <h2 className="text-lg text-primary-color font-medium overflow-hidden">{item.name}</h2>
+            <h2 className="text-lg text-primary-color font-medium overflow-hidden object-cover w-32">{item.name}</h2>
             </div>
             <h3 className="text-sm text-primary-color">{item.adresse}</h3>
             <span className="flex gap-5">
@@ -22,9 +18,6 @@ function Cards() {
             </span>
             </span>
     </article>
-    </Link>
-  ))}
-  </>
   )
 }
 
--- dev-react/src/pages/Homepage.jsx
+++ dev-react/src/pages/Homepage.jsx
@@ -1,18 +1,22 @@
+import { Link } from "react-router-dom";
 import Cards from "../components/Cards";
 import { Footer } from "../components/Footer";
 import MapComponent from "../components/MapComponent";
-
+import dataset from "../../data.json"
 
 function Homepage() {
+  const data = dataset.Sheet1 || [];
+  const limitedData = data.slice(0, 20);
   return <main>
     <section className="pb-[65vh]">
    <MapComponent />
    </section>
    <section className="flex flex-col items-center gap-5">
-  <Cards/>
-  <Cards/>
-  <Cards/>
-  <Cards/>
+   {limitedData.map((item) => (
+    <Link to={`/shop/${item.id}`} key={item.id}>
+  <Cards item={item}/>
+  </Link>
+))}
   </section>
   <Footer/>
   </main>
