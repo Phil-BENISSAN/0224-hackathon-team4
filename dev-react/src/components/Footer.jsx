5739505c3558117d6fc8433885462716213dc1ec
AXEL JANIAK <axeljpro@gmail.com>
2024-07-05 00:25:52 +0200

    last fix ohyeah

--- dev-react/src/components/Footer.jsx
+++ dev-react/src/components/Footer.jsx
@@ -1,19 +1,18 @@
 import "../components/Footer.css";
-import logo from "../assets/images/logoInclusifind.svg";
-
+import logo from "../assets/images/logotest.png";
+import insta from "../assets/images/instagram.svg"
 import tiktok from "../assets/images/tiktok.svg";
 import facebook from "../assets/images/facebook.svg";
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      <img src={logo} alt="" className="logo"/>
+      <img src={logo} alt="" className="w-36"/>
       <div className="container-footer-logos">
-        <img src="" alt="" className="svg-logo" />
+        <img src={insta} alt="" className="svg-logo" />
         <img src={tiktok} alt="" className="svg-logo" />
         <img src={facebook} alt="" className="svg-logo" />
       </div>
-      <p>Mention légales</p>
     </footer>
   );
 };
