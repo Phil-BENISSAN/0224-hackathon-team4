a824497a190179ceb3c925b734a78fd1ee7d77d3
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-07-04 17:18:33 +0200

    button

--- dev-react/src/pages/Homepage.jsx
+++ dev-react/src/pages/Homepage.jsx
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import Cards from "../components/Cards";
+import FilterButton from "../components/FilterButton";
 import MapComponent from "../components/MapComponent";
 import SearchBar from "../components/SearchBar";
+import Tags from "../components/Tags";
 
 function Homepage() {
+  const [cardShop, setCardShop] = useState("");
+  const [tagNeed, setTagNeed] = useState("");
+
+  const handleClickShop = () => {
+    setCardShop(cardShop);
+  };
+
+  const handleClickNeed = () => {
+    setTagNeed(tagNeed);
+  };
+
   return (
     <main>
       <section>
         <MapComponent />
       </section>
-        <SearchBar />
+      <SearchBar />
+      <FilterButton
+        handleClickNeed={handleClickNeed}
+        handleClickShop={handleClickShop}
+      />
+      <Tags />
       <section className="flex flex-col items-center bottom-10">
-        <Cards />
-        <Cards />
-        <Cards />
         <Cards />
       </section>
     </main>
--- dev-react/tailwind.config.js
+++ dev-react/tailwind.config.js
@@ -14,9 +14,11 @@ export default {
         "secondary-color": "#FFECEC",
         "bg-blue" : "#553EAE",
         "bg-grey" : "#E6E8E7",
+        "bg-pink" : "#FFECEC"
       },
       boxShadow: {
         "custom-shadow": "15px 15px 0px 0px var(--primaryColor)",
+        "box" : "rgba(0, 0, 0, 0.24) 0px 3px 8px;"
       },
     },
   },
