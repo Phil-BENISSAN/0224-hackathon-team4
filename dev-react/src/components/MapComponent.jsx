bbc7bffe34462eb53db992be9732a9fe0e8ce2f4
AXEL JANIAK <axeljpro@gmail.com>
2024-07-04 22:20:01 +0200

    test

--- dev-react/src/components/MapComponent.jsx
+++ dev-react/src/components/MapComponent.jsx
@@ -4,13 +4,11 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import dataset from "../../data.json";
 
-
 // Fix marker icon issue
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "./src/assets/images/marker.png",
+  iconRetinaUrl: "./src/assets/images/marker.png",
   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
@@ -18,6 +16,7 @@ L.Icon.Default.mergeOptions({
 const MapComponent = () => {
   const [position, setPosition] = useState([44.8694507, -0.565266]);
   const data = dataset.Sheet1 || [];
+  const limitedData = data.slice(0, 20);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -40,10 +39,18 @@ const MapComponent = () => {
       <Marker position={position}>
         <Popup>Vous êtes ici !</Popup>
       </Marker>
-      {data.map((item) => (
+      {limitedData.map((item) => (
         <Marker position={[item.latitude, item.longitude]} key={item.id}>
           <Popup>
-            <img className="w-32 h-32 object-cover" src={`${item.url_image}`}></img>{`${item.name}`}{`${item.activite}`}</Popup>
+            <img
+              className="w-32 h-32 object-cover"
+              src={`${item.url_image}`}
+            ></img>
+            <div className="flex flex-col">
+              <h3 className="font-medium font-primary-color">{`${item.name}`}</h3>
+              <h3>{`${item.activite}`}</h3>
+            </div>
+          </Popup>
         </Marker>
       ))}
     </MapContainer>
