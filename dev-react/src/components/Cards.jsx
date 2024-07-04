b2c2c4756aeac7c8d21df31fa56f9452b65fcf17
AXEL JANIAK <axeljpro@gmail.com>
2024-07-04 12:06:13 +0200

    modif

--- dev-react/src/components/Cards.jsx
+++ dev-react/src/components/Cards.jsx
@@ -1,12 +1,16 @@
 
 function Cards() {
   return (
-    <article className="flex outline w-[22] h-[10] p-8 m-[15%] rounded-md gap-5">
-            <img src="./src/assets/images/logo-favicon.svg" alt="test" className="w-32"/>
+    <article className="flex outline-1 w-[22rem] h-[10rem] p-8 rounded-2xl gap-3 shadow-lg items-center">
+            <img src="./src/assets/images/Plat céramique blanche.jpg" alt="test" className="w-32 h-32 rounded"/>
             <div className="flex flex-col">
-            <h2 className="">Random Name</h2>
-            <h2>Random Commerce</h2>
-            <h3>Random Adresse</h3>
+            <h2 className="text-lg text-primary-color font-medium">Random Name</h2>
+            <h3 className="text-sm text-primary-color">Random Adresse</h3>
+            <span className="flex gap-5 pt-10">
+                <img src="./src/assets/images/NoEyes.svg"></img>
+                <img src="./src/assets/images/old2.svg"></img>
+                <img src="./src/assets/images/physical-disability-01.svg"></img>
+            </span>
             </div>
     </article>
   )
--- dev-react/src/pages/Homepage.jsx
+++ dev-react/src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
 import Cards from "../components/Cards";
 import MapComponent from "../components/MapComponent";
 
+
 function Homepage() {
   return <main>
    <MapComponent />
-   <section className="flex flex-col">
+   <section className="flex flex-col items-center">
   <Cards/>
   </section>
   </main>
